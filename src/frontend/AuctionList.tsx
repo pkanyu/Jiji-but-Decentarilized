67bb7983b62189d2839308c86bde52497bf57530
pkanyu <petersonkariuki91@gmail.com>
2024-01-19 10:54:38 +0300

    This

---
+++ src/frontend/AuctionList.tsx
@@ -0,0 +1,53 @@
+import './AuctionList.scss';
+import { useEffect, useState } from "react";
+import { AuctionOverview } from "../declarations/backend/backend.did";
+import { backend } from "../declarations/backend";
+import { Link, useNavigate } from "react-router-dom";
+import { getImageSource } from './common';
+
+function AuctionList() {
+    const [list, setList] = useState<AuctionOverview[] | undefined>();
+    const navigate = useNavigate();
+    const navigationLink = (auctionId: number) => "/viewAuction/" + auctionId;
+
+    const overviewList = list?.map(overview => {
+        const id = +overview.id.toString();
+        return (
+            <li key={id} className="gallery-item" onClick={(_) => navigate(navigationLink(id))}>
+                <div className="auction-title">{overview.item.title}</div>
+                <div className="auction-description">{overview.item.description}</div>
+                {!!overview.item.image?.length && <img src={getImageSource(overview.item.image)} alt="Auction image" />}
+                <div className="gallery-item-link">
+                    <Link to={navigationLink(id)}>Item details</Link>
+                </div>
+            </li>
+        );
+    });
+
+    const fetchAuction = async () => {
+        let result = await backend.getOverviewList();
+        setList(result);
+    }
+
+    useEffect(() => {
+        fetchAuction();
+    }, []);
+
+    return (
+        <>
+            {list == null &&
+                <div className="section">Loading</div>
+            }
+            {list?.length == 0 &&
+                <div className="section">No Items listed so far</div>
+            }
+            {list != null && list.length > 0 &&
+                <ul className="gallery">
+                    {overviewList}
+                </ul>
+            }
+        </>
+    );
+}
+
+export default AuctionList;
